faed7a232e1625bd93c80f74df9cfe8f52242157
kunkka0511 <120559100+kunkka0511@users.noreply.github.com>
2023-06-15 06:39:34 -0400

    profile and verify and user check

--- lib/user/profile.dart
+++ lib/user/profile.dart
@@ -1,3 +1,4 @@
+import 'package:attendance/pages/login.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -11,67 +12,116 @@ class _ProfileState extends State<Profile> {
   final uid = FirebaseAuth.instance.currentUser!.uid;
   final email = FirebaseAuth.instance.currentUser!.email;
   final creationTime = FirebaseAuth.instance.currentUser!.metadata.creationTime;
+
+  User? user = FirebaseAuth.instance.currentUser;
+
+  Future<void> verifyEmail() async {
+    if (user != null && !user!.emailVerified) {
+      await user!.sendEmailVerification();
+      print('Verification Email has been sent');
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          backgroundColor: Colors.black26,
+          content: Text(
+            'Verification Email has been sent',
+            style: TextStyle(fontSize: 18.0, color: Colors.amber),
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 60.0),
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Image(
-              image: AssetImage('assets/images/profile.png'),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Image(
+                image: AssetImage('assets/images/profile.png'),
+              ),
             ),
-          ),
-          SizedBox(height: 50.0),
-          Column(
-            children: [
-              Text(
-                'User Id',
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                uid,
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300),
-              ),
-            ],
-          ),
-          SizedBox(height: 50.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Email: $email',
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Verify Email',
-                  style: TextStyle(fontSize: 22.0, color: Colors.lightBlue),
+            SizedBox(height: 30.0),
+            Column(
+              children: [
+                const Text(
+                  'User Id',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
                 ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 50.0,
-          ),
-          Column(
-            children: [
-              Text(
-                'Created',
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                creationTime.toString(),
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w300,
+                Text(
+                  uid,
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300),
                 ),
-              )
-            ],
-          )
-        ],
+              ],
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Email: $email',
+                  style: const TextStyle(
+                      fontSize: 18.0, fontWeight: FontWeight.w300),
+                ),
+                user!.emailVerified
+                    ? const Text(
+                        'Verified',
+                        style:
+                            TextStyle(fontSize: 22.0, color: Colors.lightBlue),
+                      )
+                    : TextButton(
+                        onPressed: () => {verifyEmail()},
+                        child: const Text(
+                          'Verify Email',
+                          style: TextStyle(
+                              fontSize: 22.0, color: Colors.lightBlue),
+                        ),
+                      )
+              ],
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Column(
+              children: [
+                const Text(
+                  'Created',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  creationTime.toString(),
+                  style: const TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w300,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => loginPage(),
+                      ),
+                      (route) => false);
+                },
+                child: const Text(
+                  'Logout',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
