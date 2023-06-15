429214f2981ec40040d8efdfc7a5c07bae4fa82a
kunkka0511 <120559100+kunkka0511@users.noreply.github.com>
2023-06-15 02:21:47 -0400

    forget

--- lib/pages/forget_pass.dart
+++ lib/pages/forget_pass.dart
@@ -1,4 +1,8 @@
+import 'package:attendance/pages/login.dart';
+import 'package:attendance/pages/signup.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class forgotPass extends StatefulWidget {
   @override
@@ -6,8 +10,163 @@ class forgotPass extends StatefulWidget {
 }
 
 class _forgotPassState extends State<forgotPass> {
+  final _formKey = GlobalKey<FormState>();
+  var email = "";
+  final emailController = TextEditingController();
+
+  resetPassword() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          backgroundColor: Colors.amber,
+          content: Text(
+            'Password Reset Email has been sent!',
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ),
+      );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => loginPage()),
+      );
+    } on FirebaseAuthException catch (error) {
+      if (error.code == 'user-not-found') {
+        print('No user found for that email');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: Colors.amber,
+            content: Text(
+              'No user found for that email',
+              style: TextStyle(fontSize: 18.0, color: Colors.amber),
+            ),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Reset Password'),
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Image(
+              image: AssetImage('assets/images/signup.png'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20.0),
+            child: const Text(
+              'Reset link will be send to your email ID!',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          Expanded(
+              child: Form(
+            key: _formKey,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+              child: ListView(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                      autofocus: false,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Colors.black26,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      controller: emailController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Email';
+                        } else if (!value.contains('@')) {
+                          return 'Please enter valid email';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              setState(() {
+                                email = emailController.text;
+                              });
+                              resetPassword();
+                            }
+                          },
+                          child: const Text(
+                            'Send email',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => loginPage(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(fontSize: 13.0),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(' Do not have an account ?'),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                PageRouteBuilder(
+                                  pageBuilder: (context, a, b) => Signup(),
+                                  transitionDuration:
+                                      const Duration(seconds: 0),
+                                ),
+                                (route) => false);
+                          },
+                          child: Text('Signup'),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ))
+        ],
+      ),
+    );
   }
 }
